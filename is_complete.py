7eb3bf803151e7e83bbffdb5d42d827558cb379c
bruunoporto <bruunoporto0@gmail.com>
2022-07-13 18:52:05 -0300

    adds timer

--- is_complete.py
+++ is_complete.py
@@ -7,8 +7,8 @@ arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
-    data = arduino.readline()
-    return data
+    # data = arduino.readline()
+    return 'send it'
 # while True:
 #     num = input("Enter a number: ") # Taking input from user
 #     value = write_read(num)
@@ -21,13 +21,13 @@ is_running = False
 
 while True:
                                                         #x    y    width height
-    if pyautogui.locateOnScreen('complete.jpeg', region=(200, 903, 1389, 27) ,  confidence=0.6) != None:
-        times_completed += 1
-        time.sleep(20)
-        print('achei o completo')
+    # if pyautogui.locateOnScreen('complete.jpeg', region=(200, 903, 1389, 27) ,  confidence=0.6) != None:
+    #     times_completed += 1
+    #     time.sleep(20)
+    #     print('achei o completo')
     print('ok')
 
-    if times_completed == 2:
+    if times_completed == 1:
         write_read(2)
         times_completed = 0
         total -= 1
@@ -39,6 +39,8 @@ while True:
         pyautogui.click(1389, 534) #the pixels of play button
         is_running = True
         print('começando a printar')
+        time.sleep(60*8)
+        times_completed = 1
     
     time.sleep(10)
 
